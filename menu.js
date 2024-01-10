9cbbacfa8a9094ad1bd5275af671435d68516768
marco-soria <marco.soria@pucp.edu.pe>
2024-01-10 18:21:58 -0500

    cesar

---
+++ menu.js
@@ -0,0 +1,13 @@
+
+
+const navtoggle = document.querySelector('.nav-toggle')
+const navmenu = document.querySelector('.nav-menu')
+
+navtoggle.addEventListener('click', () => {
+    navmenu.classList.toggle('nav-menu_visible');
+});
+
+
+
+
+
